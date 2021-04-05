278b7d373f67e0b2ae6135e768e74cd8cd9160cb
Heang Ly <heanglypa@gmail.com>
2021-04-04 23:10:39 -0400

    add seed data

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 require('colors')
 const express = require('express')
+const connectDB = require('./connection/db')
 const path = require('path')
+const Campground = require('./models/campground')
 
 const app = express()
+
+connectDB()
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -10,6 +14,15 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
   res.render('home')
+})
+
+app.get('/makecampground', async (req, res) => {
+  const camp = new Campground({
+    title: 'My Backyard',
+    description: 'Cheap Camping'
+  })
+  await camp.save()
+  res.send(camp)
 })
 
 const PORT = 3000
---
+++ connection/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    })
+    console.log('DB connected successful'.black.bgGreen)
+  } catch (err) {
+    console.log('DB connected failed'.black.bgRed)
+    console.log(err)
+  }
+}
+
+module.exports = connectDB
